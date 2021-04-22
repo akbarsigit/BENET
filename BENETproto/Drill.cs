371d02c6c438e7e45a3b413038f7c954643a18c1
Adhyaksa Zhalifunnas <adhyaksazhalifunnas@gmail.com>
2021-04-22 22:58:01 +0700

    Update Drill Class

--- BENETproto/Drill.cs
+++ BENETproto/Drill.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace BENETproto
 {
-    class Drill
+    public class Drill : Sport
     {
+        public string drillName;
+        public string drillType;
+        public string drillPreferences;
+        public string GetSportName()
+        {
+            return this.SportName;
+        }
+        public string GetPreferences()
+        {
+            return this.drillPreferences;
+        }
+        public string DrillName
+        {
+            get
+            {
+                return drillName;
+            }
+            set
+            {
+                drillName = value;
+            }
+        }
+        public string DrillType
+        {
+            get
+            {
+                return drillType;
+            }
+            set
+            {
+                drillType = value;
+            }
+        }
+        
     }
 }
