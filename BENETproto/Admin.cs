8bfc7743a9f29d1b9eb0a7dcdb24ce2d6cf33705
akbarsigit <69757628+akbarsigit@users.noreply.github.com>
2021-04-23 11:20:35 +0700

    Admin class schema

--- BENETproto/Admin.cs
+++ BENETproto/Admin.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace BENETproto
 {
-    class Admin
+    public class Admin
     {
+        //Property
+        //Get email and pass from Database
+        private string adminEmail1;
+        private string adminPassword1;
+        
+        [key]
+        public string adminEmail { get => adminEmail1; set => adminEmail1 = value; }
+        private string adminPassword { get => adminPassword1; set => adminPassword1 = value; }
+
+        public void SignUp(string email, string pass)
+        {
+            //Add information data to Database
+            //TODO
+        }
+
+        public void Login(string email, string pass)
+        {
+            //Get adminEmail and adminPassword from database
+            //Compare to the input
+            //TODO
+        }
+
+        public void AddSport(string name, string desc)
+        {
+            //Add sports to database
+            //TODO
+        }
+
+        public void AppPlaces(string name, string desc)
+        {
+            //Add places to Database
+            //TODO
+        }
+
+        public void AddDrills(string name, string desc)
+        {
+            //Add drills to database
+            //TODO
+        }
+
+        public Boolean VerifyUser()
+        {
+            //If user verified -> ok
+            //else RemoveUser()
+            //TODO
+        }
+        
+        private void RemoveUser()
+        {
+            //TODO
+        }
     }
 }
