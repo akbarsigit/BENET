5ff7422c38228de9c06a6d3e60a08c7dd2f8d780
akbarsigit <69757628+akbarsigit@users.noreply.github.com>
2021-04-23 11:22:54 +0700

    Add class schema

--- BENETproto/Recommendation.cs
+++ BENETproto/Recommendation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,16 @@ namespace BENETproto
 {
     class Recommendation
     {
+        public string Sport { get; set; }
+        public string Weather { get; }
+        public DateTime Time { get; }
+        public string Drill { get; set; }
+        public string Place { get; set; }
+
+        public string ShowRecommendation()
+        {
+            //input sport, weather, and time -> out Drill and place
+            //TODO
+        }
     }
 }
